831da03e97b52c75d57315344c241d2546e2eeef
pascalbetz <pascal.betz@simplificator.com>
2008-08-22 12:06:44 +0200

    added tests

---
+++ test/test_helper.rb
@@ -0,0 +1,35 @@
+require 'rubygems'
+require 'active_record'
+require 'test/unit'
+
+require File.join(File.dirname(__FILE__), '..', 'init')
+
+
+ActiveRecord::Base.establish_connection(  
+  :adapter  => 'sqlite3',   
+  :database => 'test.sqlite3',   
+  :timeout => 5000
+)
+
+begin
+  ActiveRecord::Base.connection.drop_table(:foos)
+rescue
+  # no such table
+end
+
+ActiveRecord::Base.connection.create_table(:foos) do |table|
+  table.string(:not_null_string_with_limit, :null => false, :limit => 10)
+  table.string(:string_with_limit, :null => true, :limit => 20)
+  table.string(:null_string_without_limit, :null => true)
+  table.boolean(:null_boolean)
+  table.boolean(:not_null_boolean, :null => false)
+  table.integer(:null_integer, :null => true)
+  table.integer(:not_null_integer, :null => false)
+  table.float(:null_float, :null => true)
+  table.float(:not_null_float, :null => false)
+end
+
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+ActiveRecord::Base.logger.level = Logger::DEBUG # change to DEBUG if you want to see something :-)
+
+require 'foo'
---
+++ test/unit/validates_constraints_test.rb
@@ -0,0 +1,102 @@
+require File.join(File.dirname(__FILE__), '..', 'test_helper')
+
+class ValidatesConstraintsTest < Test::Unit::TestCase
+  def setup()
+    #Foo.new
+  end
+  
+  def test_not_null_string_with_limit
+    Foo.send(:validates_constraints, :not_null_string_with_limit)
+    assert(valid_foo())
+    assert((not valid_foo(:not_null_string_with_limit => 'x' * 11).valid?))
+    assert(valid_foo(:not_null_string_with_limit => '').valid?)
+    assert(valid_foo(:not_null_string_with_limit => 'x' * 10).valid?)
+  end
+  def test_string_with_limit
+    Foo.send(:validates_constraints, :string_with_limit)
+    assert(valid_foo())
+    assert((not valid_foo(:string_with_limit => 'x' * 21).valid?))
+    assert(valid_foo(:string_with_limit => '').valid?)
+    assert(valid_foo(:string_with_limit => 'x' * 10).valid?)
+    
+  end
+  
+  def test_null_string_withouth_limit
+    Foo.send(:validates_constraints, :null_string_without_limit)
+    assert(valid_foo.valid?)
+    assert(valid_foo(:null_string_without_limit => '').valid?)
+    assert(valid_foo(:null_string_without_limit => 'x' * 5).valid?)
+    assert(valid_foo(:null_string_without_limit => 'x' * 255).valid?)
+    # sqllite still adds a limit for string columns (255 chars)
+    assert((not valid_foo(:null_string_without_limit => 'x' * 256).valid?))
+  end
+  
+  def test_null_boolean
+    Foo.send(:validates_constraints, :null_boolean)
+    assert(valid_foo.valid?)
+    assert(valid_foo(:null_boolean => true).valid?)
+    assert(valid_foo(:null_boolean => false).valid?)
+  end
+
+  def test_not_null_boolean
+    Foo.send(:validates_constraints, :not_null_boolean)
+    assert(valid_foo.valid?)
+    assert(valid_foo(:not_null_boolean => true).valid?)
+    assert(valid_foo(:not_null_boolean => false).valid?)
+    assert((not valid_foo(:not_null_boolean => nil).valid?))
+  end
+  
+  def test_null_integer
+    Foo.send(:validates_constraints, :null_integer)
+    assert(valid_foo.valid?)
+    assert(valid_foo(:null_integer => nil).valid?)
+    assert(valid_foo(:null_integer => 1).valid?)
+    assert(valid_foo(:null_integer => 0).valid?)
+    assert(valid_foo(:null_integer => -1).valid?)
+  end
+
+  def test_not_null_integer
+    Foo.send(:validates_constraints, :not_null_integer)
+    assert(valid_foo.valid?)
+    assert((not valid_foo(:not_null_integer => nil).valid?))
+    assert(valid_foo(:not_null_integer => 1).valid?)
+    assert(valid_foo(:not_null_integer => 0).valid?)
+    assert(valid_foo(:not_null_integer => -1).valid?)
+  end 
+  
+    def test_null_float
+    Foo.send(:validates_constraints, :null_float)
+    assert(valid_foo.valid?)
+    assert(valid_foo(:null_float => nil).valid?)
+    assert(valid_foo(:null_float => 1.0).valid?)
+    assert(valid_foo(:null_float => 0.0).valid?)
+    assert(valid_foo(:null_float => -1.0).valid?)
+  end
+
+  def test_not_null_float
+    Foo.send(:validates_constraints, :not_null_float)
+    assert(valid_foo.valid?)
+    assert((not valid_foo(:not_null_float => nil).valid?))
+    assert(valid_foo(:not_null_float => 1.0).valid?)
+    assert(valid_foo(:not_null_float => 0.0).valid?)
+    assert(valid_foo(:not_null_float => -1.0).valid?)
+  end 
+  private
+  
+  def valid_foo(options = {})
+    valid = {
+      :null_string_without_limit => '', 
+      :not_null_string_with_limit => '',
+      :string_with_limit => '',
+      :null_boolean => nil,
+      :not_null_boolean => false,
+      :null_integer => nil,
+      :not_null_integer => 123,
+      :null_float => nil,
+      :not_null_float => 123.0
+    }
+    Foo.new(valid.merge(options))
+    
+  end
+  
+end
